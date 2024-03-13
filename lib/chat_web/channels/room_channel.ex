3c1fcf096ab6062c5a05d8a6be45a70fa3bd1182
boldPro <probold777@gmail.com>
2024-03-12 21:19:57 -0400

    Connected db and it saves msgs in there

--- lib/chat_web/channels/room_channel.ex
+++ lib/chat_web/channels/room_channel.ex
@@ -2,8 +2,9 @@ defmodule ChatWeb.RoomChannel do
   use ChatWeb, :channel
 
   @impl true
-  def join("room:lobby", payload, socket) do
+  def join("room:lobby", _payload, socket) do
     # if authorized?(payload) do
+      send(self(), :after_join)
       {:ok, socket}
     # else
       # {:error, %{reason: "unauthorized"}}
@@ -21,6 +22,7 @@ defmodule ChatWeb.RoomChannel do
   # broadcast to everyone in the current topic (room:lobby).
   @impl true
   def handle_in("shout", payload, socket) do
+    Chat.Message.changeset(%Chat.Message{}, payload) |> Chat.Repo.insert
     broadcast(socket, "shout", payload)
     {:noreply, socket}
   end
@@ -29,4 +31,18 @@ defmodule ChatWeb.RoomChannel do
   # defp authorized?(_payload) do
   #   true
   # end
+
+  @impl true
+def handle_info(:after_join, socket) do
+  Chat.Message.get_messages()
+  |> Enum.reverse() # revers to display the latest message at the bottom of the page
+  |> Enum.each(fn msg -> push(socket, "shout", %{
+      name: msg.name,
+      message: msg.message,
+      inserted_at: msg.inserted_at,
+    }) end)
+  {:noreply, socket} # :noreply
+end
+
+
 end
